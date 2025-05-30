0bcc3c7ff0f4871768f949d63574ce1cd3d7c8c4
Hirdy0903 <129482022+Hirdy0903@users.noreply.github.com>
2025-05-30 04:25:23 +0000

    hello world with react

---
+++ app.js
@@ -0,0 +1,5 @@
+  console.log("app.js loaded");
+
+   const heading = React.createElement("h1",{},"hello world from react");
+    const root= ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
