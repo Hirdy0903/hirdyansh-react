0e3dd87b952b2578b79aaa4083fe32abca8ef7ce
Hirdy0903 <129482022+Hirdy0903@users.noreply.github.com>
2025-05-30 13:05:49 +0000

    folder ptoblem

--- lec01/app.js
+++
@@ -1,5 +0,0 @@
-  console.log("app.js loaded");
-
-   const heading = React.createElement("h1",{},"hello world from react");
-    const root= ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
