4fa0b7001a544c3a23ed1e056f29f59517e1e66f
Hirdy0903 <129482022+Hirdy0903@users.noreply.github.com>
2025-05-30 17:27:21 +0000

    episode 2 namaste react

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 {/* <div id="parent">
     <div id="child">
         <h1>
@@ -6,10 +8,12 @@
         <h2></h2>
     </div>
 </div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"i am h1 tag"),
+        [React.createElement("h1",{},"namaste react"),
             React.createElement("h1",{},"i am h2 tag")]
     )
 );
