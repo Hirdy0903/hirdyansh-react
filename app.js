87cfc48fd9691952d8ed5cd921bd646832a06ea9
Hirdy0903 <129482022+Hirdy0903@users.noreply.github.com>
2025-05-30 13:29:07 +0000

    nested childs

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
         <h1>
             
         </h1>
+        <h2></h2>
     </div>
 </div> */}
 
 const parent=React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
-        React.createElement("h1",{},"i am h1 tag")
+        [React.createElement("h1",{},"i am h1 tag"),
+            React.createElement("h1",{},"i am h2 tag")]
     )
 );
 const root=ReactDOM.createRoot(document.getElementById("root"));
