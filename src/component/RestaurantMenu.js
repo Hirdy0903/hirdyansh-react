d364c4ce5dc2942104331f786fdfe1c8f7580589
Hirdyansh <hirdyanshsaxena7@gmail.com>
2025-06-10 20:01:07 +0530

    lec07  push last

--- src/component/RestaurantMenu.js
+++ src/component/RestaurantMenu.js
@@ -1,38 +1,39 @@
-
 import { useEffect, useState } from "react";
 import Shimmer from "./shimmer";
 
-const RestaurantMenu=()=>{
-    const[resInfo,setResInfo]= useState(null);
-    useEffect(()=>{
+const RestaurantMenu = () => {
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
         fetchMenu();
+    }, []);
 
-    },[])
-    const fetchMenu=async ()=>{
-        const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7030425&lng=77.430373&restaurantId=336714&catalog_qa=undefined&submitAction=ENTER");
-    const json=await data.json();
-    console.log(json);
-    setResInfo(json.data);
-    
-    
+    const fetchMenu = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7030425&lng=77.430373&restaurantId=336714&catalog_qa=undefined&submitAction=ENTER"
+        );
+        const json = await data.json();
+        console.log(json);
+        setResInfo(json.data);
+        // If you want to see the heading text in console:
+        console.log(json.data.cards[0]?.card?.card?.text);
     };
-    if(resInfo===null){
-        return <Shimmer/>
 
+    if (resInfo === null) {
+        return <Shimmer />;
     }
+
     return (
         <div className="menu">
-<h1>{resInfo?.data?.cards[0]?.card?.card?.text}</h1>
-<h2> Menu</h2>
-<ul>
-    <li>Biryani</li>
-    <li> Burgers</li>
-    <li>Diet Coke</li>
-</ul>
-
-
-
+            <h1>{resInfo?.cards[0]?.card?.card?.text}</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Burgers</li>
+                <li>Diet Coke</li>
+            </ul>
         </div>
     );
 };
+
 export default RestaurantMenu;
